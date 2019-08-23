1b8610f79d94536f5a1102a45112686b4886a6cd
RocioAl <koroinice@gmail.com>
2019-08-23 12:10:59 -0500

    Updates

---
+++ precache-manifest.de9abd3316a3bef6554fdcffc0ffe823.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ea2b42259c589212543973c10ebb35c",
+    "url": "/LIM009-fe-burger-queen/index.html"
+  },
+  {
+    "revision": "5b33cffb6acc72261462",
+    "url": "/LIM009-fe-burger-queen/static/css/main.d2598d18.chunk.css"
+  },
+  {
+    "revision": "819ff2864bec56b26e0b",
+    "url": "/LIM009-fe-burger-queen/static/js/2.20f7965c.chunk.js"
+  },
+  {
+    "revision": "5b33cffb6acc72261462",
+    "url": "/LIM009-fe-burger-queen/static/js/main.c60f5485.chunk.js"
+  },
+  {
+    "revision": "4ec68e84b8b177095eae",
+    "url": "/LIM009-fe-burger-queen/static/js/runtime~main.63907d42.js"
+  }
+]);
